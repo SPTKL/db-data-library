8d2349951a07bca7f7db36cc9d5dff691c60e1b7
sptkl <caobaiyue@gmail.com>
2021-01-21 16:53:54 +0000

    first commit - setting up the repository

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # db-data-library
+
+## Instructions:
+1. Make sure you have GDAL installed (we are using version `2.4.0+dfsg-1+b1`)
+```bash
+sudo apt install -y gdal-bin libgdal-dev python3-gdal
+```
+2. then install poetry 
+```bash
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+```
+3. Use poetry to install dependencies `poetry install`
+3. Check out what's available via the cli `poetry run library --help`
---
+++ library/cli.py
@@ -0,0 +1,30 @@
+import time
+import typer
+import os
+
+app = typer.Typer()
+
+@app.command()
+def ingest(name: str, config: str):
+    """
+    ingest a dataset from source to destination
+    """
+    typer.echo(f"ingesting {name} using {config} ...")
+
+@app.command()
+def show(name: str):
+    """
+    show versions available for given dataset
+    """
+    typer.echo(f"showing {name} ...")
+
+@app.command()
+def load(name: str):
+    """
+    load a dataset from s3/database to a build database
+    """
+    typer.echo(f"loading {name} to database ...")
+
+def run() -> None:
+    """Run commands."""
+    app()
