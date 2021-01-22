4908c99ade0ebebe7be0c39fad9b82d056b7e796
mgraber <mgraber@planning.nyc.gov>
2021-01-22 15:56:53 +0000

    Add s3 functions

--- library/s3.py
+++ library/s3.py
@@ -1,6 +1,6 @@
 import logging
 import boto3
-from botocore.exceptions import ClientError
+from botocore.exceptions import ClientError, ParamValidationError
 from pathlib import Path
 from .progressbar import ProgressPercentage
 from . import pp
@@ -55,3 +55,67 @@ class S3:
             return [content['Key'] for content in contents]
 
     # https://s3fs.readthedocs.io/en/latest/api.html?highlight=listdir#s3fs.core.S3FileSystem.info
+
+    def info(self, key:str) -> dict:
+        """
+        Get header info for a given file
+        """
+        response = self.client.head_object(
+                    Bucket=self.bucket,
+                    Key=key,
+                )
+        return response
+
+    def cp(self, source_key:str, dest_key:str, acl: str = "public-read", info:bool = False) -> dict:
+        """
+        Copy a file to a new path within the same bucket
+
+        Parameters
+        ----------
+        key: path within the bucket of the file to copy
+        dest_ket: new path for the copy
+        acl: acl for newly created file
+        """
+        try:
+            response = self.client.copy_object(
+                        Bucket=self.bucket
+                        CopySource={
+                            'Bucket': self.bucket,
+                            'Key': source_key
+                        }
+                        ACL=acl,
+                    )
+            if info:
+                return self.info(key=dest_key)
+            return
+        except ParamValidationError as e:
+            raise ValueError(f"Copy {source_key} -> {dest_key} failed: {e}") from e
+
+    def rm(self, key:str):
+        """
+        Remove a file within the bucket
+        """
+        response = self.client.delete_object(
+                        Bucket=self.bucket,
+                        Key=key,
+                    )
+        return
+
+    def mv(self, source_key:str, dest_key:str, acl: str = "public-read", info:bool = False):
+        """
+        Move a file to a new path within the same bucket.
+        Calls cp then rm
+
+        Parameters
+        ----------
+        source_key: path within the bucket of the file to move
+        dest_ket: new path for the copy
+        acl: acl for newly created file
+        info: if true, get info for file in its new location
+        """
+        try:
+            response = self.cp(source_key=source_key, dest_key=dest_key, acl=acl)
+            response = self.rm(key=source_key)
+            if info:
+                return self.info(key=dest_key)
+        return
--- tests/test_s3.py
+++ tests/test_s3.py
@@ -10,10 +10,80 @@ from library import (
 s3 = S3(aws_access_key_id, aws_secret_access_key, aws_s3_endpoint, aws_s3_bucket)
 
 def test_s3_upload_file():
+    # Make sure file doesn't already exist
+    assert s3.exists('test/20210121/test.yml') == False
+
+    # Attempt to upload to 20210121/test.yml
     s3.upload_file(name='test', version='20210120', path=f"{Path(__file__).parent}/data/socrata.yml")
+
+    # Make sure file now exists
     assert s3.exists('test/20210121/test.yml') == True
+
     # s3.upload_file(name='test', version='20210121', path=f"{Path(__file__).parent}/data/socrata.yml", acl='private')
     # assert s3.check_existence('test/20210121/test.yml') == True
 
 def test_s3_ls():
     s3.ls('test')
+
+def test_s3_info():
+    # Make sure file exists before trying to pull info
+    assert s3.exists('test/20210121/test.yml') == True
+
+    # Pull file info
+    info = s3.info(key='test/20210121/test.yml')
+    if info:
+        print(info)
+    else:
+        print('File exists, but no info retrieved.')
+
+def test_s3_cp():
+    # Make sure the 20210121 version exists, but the latest version doesn't
+    assert s3.exists('test/20210121/test.yml') == True
+
+    # Copy 20210121 to latest
+    s3.cp(source_key='test/20210121/test.yml', dest_key='test/latest/test.yml')
+
+    # Make sure the 20210121 version exists, and the latest version doesn't
+    assert s3.exists('test/20210121/test.yml') == True
+    assert s3.exists('test/latest/test.yml') == True
+
+def test_s3_mv():
+    # Make sure that the 20210121 version exists, but the moved version doesn't
+    assert s3.exists('test/20210121/test.yml') == True
+    assert s3.exists('test/moved/test.yml') == False
+
+    # Move the 20210121 version to moved
+    s3.mv(source_key='test/20210121/test.yml', dest_key='test/moved/test.yml')
+
+    # Make sure that the moved version exists, but the 20210121 version doesn't
+    assert s3.exists('test/moved/test.yml') == True
+    assert s3.exists('test/20210121/test.yml') == False
+
+def test_s3_rm():
+    # Make sure that the moved version exists prior to move
+    assert s3.exists('test/moved/test.yml') == True
+
+    # Remove file
+    s3.rm('test/moved/test.yml')
+
+    # Make sure the file no longer exists
+    assert s3.exists('test/moved/test.yml') == False
+
+if __name__ == "__main__":
+    print("Test upload...")
+    test_s3_upload_file()
+
+    print("Test ls...")
+    test_s3_ls()
+
+    print("Test info...")
+    test_s3_info()
+
+    print("Test cp...")
+    test_s3_cp()
+
+    print("Test mv...")
+    test_s3_mv()
+
+    print("Test rm...")
+    test_s3_rm()
