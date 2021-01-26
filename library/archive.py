d436fa4fb292a4c4ed71762b246d8e83e0457170
sptkl <caobaiyue@gmail.com>
2021-01-26 16:22:41 +0000

    adding documentation to archive call class

--- library/archive.py
+++ library/archive.py
@@ -14,6 +14,9 @@ from .s3 import S3
 
 class Archive:
     def __init__(self):
+        """
+        Initialize the Archive class instance
+        """
         self.ingestor = Ingestor()
         self.s3 = S3(
             aws_access_key_id, aws_secret_access_key, aws_s3_endpoint, aws_s3_bucket
@@ -21,14 +24,48 @@ class Archive:
 
     def __call__(
         self,
-        path,
-        output_format,
+        path: str,
+        output_format: str,
         push: bool = False,
         clean: bool = False,
         latest: bool = False,
         *args,
         **kwargs
     ):
+        """
+        The `__call__` method allows a user to call a class instance with parameters.
+
+        Parameters
+        ----------
+        path: path to the configutation file
+        output_format: currently supported formats: `'csv'`, `'geojson'`, `'shapefile'`, `'postgres'`
+        push: if `True` then push to s3
+        clean: if `True`, the temporary files created under `.library` will be removed
+        latest: if `True` then tag this current version we are processing to be the `latest`
+
+        Sample Usage
+        ----------
+        ### A flat file example:
+        ```python
+        from library.archive import Archive
+        a = Archive()
+        a(
+            "path/to/config.yml",
+            output_format="csv", push=True, clean=True,
+            latest=True, compress=True,
+        )
+        ```
+        ### A postgres example:
+        > by default, for postgres `push`, `clean`, `latest`, `compress` are all default to `False`
+        ```python
+        postgres_url='postgresql://user:password@hose/db'
+        a(
+            "path/to/config.yml",
+            output_format="postgres",
+            postgres_url=postgres_url,
+        )
+        ```
+        """
         # Get ingestor by format
         ingestor_of_format = getattr(self.ingestor, output_format)
 
