303e4d10f735393fc8a12c3aa2004aa8ddf119b1
sptkl <caobaiyue@gmail.com>
2021-02-11 15:32:19 +0000

    fix socrata sourced datasets

--- library/utils.py
+++ library/utils.py
@@ -37,9 +37,6 @@ def format_url(path: str, subpath: str) -> str:
     path = path[:-1] if path[-1] == "/" else path
     url = path if len(subpath) == 0 else path + "/" + subpath
 
-    if "http" in url and ".zip" not in url:
-        return "/vsicurl/" + url
-
     if ".zip" in url:
         if "http" in url:
             url = "/vsizip/vsicurl/" + url
@@ -51,3 +48,5 @@ def format_url(path: str, subpath: str) -> str:
 
     if "s3://" in url and ".zip" not in url:
         return url.replace("s3://", "/vsis3/")
+
+    return url
