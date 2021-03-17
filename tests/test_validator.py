4a91bc53670d92e0a7ca1d66969b23efea7236a3
Ruben Vecino <rubenvec95@gmail.com>
2021-03-17 18:27:46 +0000

    Added @property decorator to dataset_name_matches

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -1,17 +1,23 @@
 from pathlib import Path
-from library.validator import Validator
+
 import yaml
+
+from library.validator import Validator
 
 v = Validator(f"{Path(__file__).parent}/data/test_none.yml")
 
+
 def test_tree_structure():
     assert v.tree_is_valid
-    
+
+
 def test_dataset_name_matches():
-    assert v.dataset_name_matches('test_none')
+    assert v.dataset_name_matches
+
 
 def test_has_only_one_source():
     assert v.has_only_one_source
+
 
 def test_file_is_valid():
     assert v.file_is_valid
