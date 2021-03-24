3a47fb8ef571e4b1d6656e49e57b2546d53c169d
Ruben Vecino <rubenvec95@gmail.com>
2021-03-24 15:49:57 +0000

    Removed dataset_name_matches

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -11,10 +11,6 @@ def test_tree_structure():
     assert v.tree_is_valid
 
 
-def test_dataset_name_matches():
-    assert v.dataset_name_matches
-
-
 def test_has_only_one_source():
     assert v.has_only_one_source
 
