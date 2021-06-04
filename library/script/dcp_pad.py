6d43be9e881bc4e615ec2ce07cbc8d1689b31bd8
mgraber <mgraber@planning.nyc.gov>
2021-06-04 18:10:08 +0000

    Use requests

--- library/script/dcp_pad.py
+++ library/script/dcp_pad.py
@@ -1,3 +1,4 @@
+import os
 from zipfile import ZipFile
 
 import pandas as pd
@@ -17,13 +18,18 @@ class Scriptor:
     def ingest(self) -> pd.DataFrame:
 
         url = f"https://www1.nyc.gov/assets/planning/download/zip/data-maps/open-data/pad{self.version}.zip"
-        resp = requests.get(url)
-        with ZipFile(file_name, "r") as zip:
-            zip.extract(f"pad{self.version}/bobaadr.txt")
+        r = requests.get(url, stream=True)
+        with open(f"pad{self.version}.zip", "wb") as fd:
+            for chunk in r.iter_content(chunk_size=128):
+                fd.write(chunk)
+        with ZipFile(f"pad{self.version}.zip", "r") as zip:
+            zip.extract("bobaadr.txt")
             df = pd.read_csv("bobaadr.txt", dtype=str)
         return df
 
     def runner(self) -> str:
         df = self.ingest()
         local_path = df_to_tempfile(df)
+        os.remove(f"pad{self.version}.zip")
+        os.remove("bobaadr.txt")
         return local_path
