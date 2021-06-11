14e10900d7c7448751f431c831090e11aee6f6a6
sptkl <caobaiyue@gmail.com>
2021-06-11 15:10:48 +0000

    avoid overwriting of the same file, use random file name

--- library/script/doitt_buildingfootprints.py
+++ library/script/doitt_buildingfootprints.py
@@ -1,4 +1,6 @@
 import os
+import random
+import string
 
 import requests
 
@@ -6,6 +8,12 @@ import requests
 class Scriptor:
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
+
+    @property
+    def filename(self):
+        letters = string.ascii_lowercase
+        random_string = "".join(random.choice(letters) for i in range(10))
+        return f"{random_string}.zip"
 
     @property
     def metadata(self):
@@ -20,7 +28,7 @@ class Scriptor:
         return f"https://data.cityofnewyork.us/api/geospatial/{self.uid}\?method=\export\&format=\Shapefile"
 
     def runner(self) -> str:
-        local_path = "buildingfootprints.zip"
+        local_path = self.filename
         print("Downloading from url:\n", self.url)
         os.system(f"curl -o {local_path} {self.url}")
         return local_path
