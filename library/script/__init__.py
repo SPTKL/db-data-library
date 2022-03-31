3f58f9b77a2afeef000093912588b5552c247b86
Te Du <tedudevelop@gmail.com>
2022-03-31 14:24:25 -0400

    complete else statement in output

--- library/script/__init__.py
+++ library/script/__init__.py
@@ -7,5 +7,6 @@ def df_to_tempfile(df: pd.DataFrame, encode_utf_8=False) -> str:
     f = tempfile.NamedTemporaryFile(delete=False, suffix=".csv")
     if encode_utf_8:
         df.to_csv(f, encoding="utf-8", index=False)
-    df.to_csv(f, index=False)
+    else:
+        df.to_csv(f, index=False)
     return f.name
