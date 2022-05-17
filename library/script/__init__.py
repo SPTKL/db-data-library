da4b9a29a7fa20d2005cc17a509ee20cf5006ed1
Te Du <tedudevelop@gmail.com>
2022-05-17 14:21:00 +0000

    remove utf 8 flag since it is by default for to_csv

--- library/script/__init__.py
+++ library/script/__init__.py
@@ -3,7 +3,7 @@ import tempfile
 import pandas as pd
 
 
-def df_to_tempfile(df: pd.DataFrame, encode_utf_8=False) -> str:
+def df_to_tempfile(df: pd.DataFrame) -> str:
     f = tempfile.NamedTemporaryFile(delete=False, suffix=".csv")
     df.to_csv(f, index=False)
     return f.name
