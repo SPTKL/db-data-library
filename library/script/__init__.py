3e420e2af23bed4d6ab882f9e7b44e54624e5113
Te Du <tedudevelop@gmail.com>
2022-05-17 14:16:56 +0000

    set up boto3 workflow to read in from edm private

--- library/script/__init__.py
+++ library/script/__init__.py
@@ -5,8 +5,5 @@ import pandas as pd
 
 def df_to_tempfile(df: pd.DataFrame, encode_utf_8=False) -> str:
     f = tempfile.NamedTemporaryFile(delete=False, suffix=".csv")
-    if encode_utf_8:
-        df.to_csv(f, encoding="utf-8", index=False)
-    else:
-        df.to_csv(f, index=False)
+    df.to_csv(f, index=False)
     return f.name
--- library/script/dob_now_applications.py
+++ library/script/dob_now_applications.py
@@ -1,19 +1,45 @@
+from multiprocessing import connection
 import pandas as pd
 
 from . import df_to_tempfile
+from dotenv import load_dotenv
+import boto3
+import os
 
+from io import StringIO
+
+# Load environmental variables
+load_dotenv()
 
 class Scriptor:
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
 
+    @property
+    def version(self):
+        return self.config["dataset"]["version"]
+
     def ingest(self) -> pd.DataFrame:
-        df = pd.read_csv("s3://edm-private/dob_now/DOB_Now_Job_Filing_Data_for_DCP_{{ version }}.csv", encoding="Windows-1252")
+        client = boto3.client(
+            "s3",
+            aws_access_key_id = os.environ["AWS_ACCESS_KEY_ID"],
+            aws_secret_access_key = os.environ["AWS_SECRET_ACCESS_KEY"],
+            endpoint_url = os.environ["AWS_S3_ENDPOINT"]
+        )
+        obj = client.get_object(
+            Bucket="edm-private",
+            Key=f"dob_now/DOB_Now_Job_Filing_Data_for_DCP_{self.version}.csv",
+        )
+        #convert to a literal string of bytes with correct encoding
+        s = str(obj["Body"].read(), "Windows-1252")
+        # use StringIO to convert consumable format for pd.read_csv
+        data = StringIO(s)
+        df = pd.read_csv(data, encoding="Windows-1252")
         return df
 
     def runner(self) -> str:
         df = self.ingest()
-        local_path = df_to_tempfile(df, encode_utf_8=True)
+        local_path = df_to_tempfile(df)
         return local_path
 
 
